df54ccf38f835e4262677dfd9059b25340135016
T Bird <tompcapers@gmail.com>
2021-06-03 18:47:33 -0700

    added initial api call

--- script.js
+++ script.js
@@ -17,4 +17,19 @@ function searchCitySubmit(event) {
 
   }
 
+  function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&CA&appid=d5c3f6289a5941c4842a8f2f928b3c55';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+      });
+
+    }
+
 searchFormEl.addEventListener('submit', searchCitySubmit);
+searchFormEl.addEventListener('submit', getApi);
